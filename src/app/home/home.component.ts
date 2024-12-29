d46b63c7df007c479da86b1fc061fddae65bf4fd
Md. Shahajalal <shaha.jalal@streamstech.com>
2024-12-29 09:50:44 +0600

    Building the user interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,22 +12,12 @@ import { RegisterComponent } from '../register/register.component';
 export class HomeComponent {
   http = inject(HttpClient);
   registerMode = false;
-  users: any;
   
-  ngOnInit(): void {
-    this.getUsers();
-  }
   registerToggle() {
     this.registerMode = !this.registerMode
   }
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
-  getUsers() {
-    this.http.get('http://localhost:5000/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
 }
